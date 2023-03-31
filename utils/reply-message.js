f3b417929a1db2f11b06f74e703cab7c6c9dc9c2
TaoistSHIH <sweety821210@gmail.com>
2023-03-31 19:04:20 +0800

    Update reply-message.js

--- utils/reply-message.js
+++ utils/reply-message.js
@@ -1,12 +1,16 @@
 import config from '../config/index.js';
 import { reply } from '../services/line.js';
-
+var ss = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1cJLdguzutVWYOw2WQwBe6sitrwGkDH8M6fkVgP8SSTk/edit#gid=0");//雲端試算表連結
+var sheet = ss.getSheetByName("工作表1");//表單名稱
 const replyMessage = ({
   replyToken,
   messages,
 }) => {
   if (config.APP_ENV !== 'production') return { replyToken, messages };
   return reply({ replyToken, messages });
+   
+ sheet.appendRow([replyToken,userMessage,userid,returnmessage[0].text]);
+
 };
 
 export default replyMessage;
